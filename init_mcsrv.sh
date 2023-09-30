f9f7a8d2bf801c3be45a692bb33a8b04c4905386
Jesse Zhao <106645580+JesseZhao11@users.noreply.github.com>
2023-09-30 21:43:05 +0800

    Update init_mcsrv.sh

--- init_mcsrv.sh
+++ init_mcsrv.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-MAX_MEMORY="1G"
-echo ********************************建议使用 Rocky Linux 9 最新版本并使用 root 用户执行此脚本********************************
-sleep 5s
-echo ********************************即将开始部署 Minecraft 1.20.1服务器********************************
-sleep 5s
+MAX_MEMORY="4G"
+echo ****************必须使用 Rocky Linux 用户执行此脚本****************
+echo ****************建议使用 root 用户执行此脚本****************
+echo ****************即将在3秒后开始部署 Minecraft Spigot 1.19.4 服务器****************
+sleep 3s
 systemctl disable --now firewalld
 sed -ri 's/^(SELINUX=).*/\1disabled/' /etc/selinux/config
-mkdir /minecraft
-mkdir /minecraft/server /minecraft/backups
+mkdir -p /minecraft/server
 cd /minecraft/server || exit
-dnf -y upgrade
-dnf -y install wget java-17-openjdk-headless
-wget http://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
-touch start.sh
-echo java -jar -Xmx$MAX_MEMORY -server -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M server.jar nogui >> start.sh
+sudo dnf -y upgrade
+sudo dnf -y install wget java-17-openjdk-headless
+sudo dnf -y autoremove
+wget https://download.getbukkit.org/spigot/spigot-1.19.4.jar
+echo java -Xmx$MAX_MEMORY -jar spigot-1.19.4.jar --nogui >> start.sh
 chmod a+x start.sh
 ./start.sh
 sed -i '3c\eula=true' eula.txt
-echo ****************输入./start.sh以启动您的Minecraft 1.20.1服务器****************
+sed -i '23c\online-mode=false' server.properties
+cd /minecraft/server || exit
+echo ****************输入./start.sh以启动您的 Minecraft Spigot 1.19.4 服务器****************
 echo ****************启动后在游戏中输入服务器的IP地址以访问 Minecraft 世界****************
-echo ****************有任何问题请加QQ:3255838070或在我的 Github 主页中反馈****************
+echo ****************有任何问题请加QQ:3255838070****************
