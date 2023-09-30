a31a0395d4b58bf7a1cc4e797cb3b5672cddf2ee
Jesse Zhao <106645580+JesseZhao11@users.noreply.github.com>
2023-09-30 21:46:07 +0800

    Update init_nginxsrv_by_dnf.sh

--- init_nginxsrv_by_dnf.sh
+++ init_nginxsrv_by_dnf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSION="1.24.0"
 echo ********************************建议使用 Rocky Linux 9 最新版本并使用 root 用户执行本脚本********************************
-echo ********************************即将开始安装并开机启动 Nginx 1.24.0********************************
+echo ********************************即将在3秒后开始安装并开机启动 Nginx 1.24.0********************************
 sleep 3s
 systemctl disable --now firewalld
 sed -ri 's/^(SELINUX=).*/\1disabled/' /etc/selinux/config
@@ -19,6 +19,7 @@ make install
 cd /apps/nginx-$VERSION/sbin || exit
 rm -rf /usr/local/src/nginx-$VERSION
 ./nginx
+cd
 echo ****************Nginx 1.24.0 部署完成****************
 echo ****************请通过您的浏览器上输入P+80端口以访问 Nginx 默认页面****************
 echo ****************有任何问题请加QQ:3255838070或在我的 Github 主页中反馈****************
